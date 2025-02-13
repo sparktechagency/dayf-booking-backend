d1493e993fc59334ed6846a660853267cc63c574
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2025-02-12 23:15:31 -0500

    first commit

---
+++ src/app/modules/user/user.constants.ts
@@ -0,0 +1,11 @@
+export const USER_ROLE = {
+  super_admin: 'super_admin',
+  sub_admin: 'sub_admin',
+  admin: 'admin',
+  user: 'user', 
+};
+
+export const gender = ['Male', 'Female', 'Others'];
+export const Role = ['admin', 'super_admin', 'sub_admin', 'user'];
+
+export const userSearchableFields = ['shopId', 'email'];
