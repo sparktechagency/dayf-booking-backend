f424bec0e8c48dea2afdace8cf7466a7fb0a021d
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2025-09-04 18:08:52 +0600

    error fixeing

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: false,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   }),
