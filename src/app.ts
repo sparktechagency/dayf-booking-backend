bd6d0a81d13241a7fb4bd5f12496df00ee1eadbd
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2025-09-04 20:00:31 +0600

    [Done]payment releted problem fixing

--- src/app.ts
+++ src/app.ts
@@ -28,22 +28,22 @@ app.use(cookieParser());
 //   }),
 // );
 
-// app.use(
-//   cors({
-//     origin: [
-//       'https://admin.techcrafters.tech',
-//       'https://www.admin.techcrafters.tech',
-//       'https://api.techcrafters.tech',
-//       'https://www.api.techcrafters.tech',
-//       'https://socket.techcrafters.tech',
-//       'https://www.socket.techcrafters.tech',
-//       'https://techcrafters.tech',
-//       'https://www.techcrafters.tech',
-//     ],
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//   }),
-// );
+app.use(
+  cors({
+    origin: [
+      'https://admin.techcrafters.tech',
+      'https://www.admin.techcrafters.tech',
+      'https://api.techcrafters.tech',
+      'https://www.api.techcrafters.tech',
+      'https://socket.techcrafters.tech',
+      'https://www.socket.techcrafters.tech',
+      'https://techcrafters.tech',
+      'https://www.techcrafters.tech',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  }),
+);
 // Remove duplicate static middleware
 // app.use(app.static('public'));
 
